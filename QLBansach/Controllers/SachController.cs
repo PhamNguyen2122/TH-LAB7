cd55ee9e63ed6d735ecf726419b56aaf79866a2e
PhamNguyen2122 <85982805+PhamNguyen2122@users.noreply.github.com>
2021-07-16 23:26:57 +0700

    Uploat Project

---
+++ QLBansach/Controllers/SachController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLBansach.Models;
+
+public class SachController : ApiControllerAttribute
+{
+    SachCla[] sach = new SachCla[]
+
+    {
+        new SachCla
+            {
+                Id= 1,
+                Title="Tôi thấy hoa vàng trên cỏ xanh",
+                AuthorName="Pham Nguyen",
+                Price=1,
+                Content="truyện kể về tuổi thơ..."
+            },
+            new SachCla
+            {
+                Id= 2,
+                Title="Pro ASP.NET MVC5",
+                AuthorName="Administrator",
+                Price=3.5M,
+                Content="the ASP.MVC 5 Framework is the latest evolution of Microshoft's ASP.NET web platform"
+            }
+    };
+public IHttpActionResult GetSach(int id)
+{
+    var sach = sach.FirstOrDeFault((p) => p.Id == id);
+    if (sach == null)
+    {
+        return NotFound();
+    }
+    { 
+    return Ok(Sach);
+    }
+}
---
+++ QLBansach/Models/SachCla.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace QLBansach.Models
+{
+    public class SachCla
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public Decimal Price { get; set; }
+    
+    }
+}
